5dc6212fcb07f5f21b2a98c3c8f4e2e3001ed40b
YIN YU HANG <yhyin@YINs-MacBook-Air.local>
2024-11-13 23:19:33 +0800

    modified insert html

--- tests/test_insert_html.py
+++ tests/test_insert_html.py
@@ -10,7 +10,7 @@ def test_insert_html():
     f1 = "tests/test_html/insert_out.html"
 
     commands = [
-        "read tests/test_html/insert.html",
+        "read tests/test_html/test.html",
         'insert li item2 item3 "Item B"',
         'insert li item1 item2 "Item A"',
         "save tests/test_html/insert_out.html",
@@ -23,5 +23,7 @@ def test_insert_html():
 
     h0 = create_file_hash(f0)
     h1 = create_file_hash(f1)
+
+    os.remove(f1)
 
     assert h0 == h1
